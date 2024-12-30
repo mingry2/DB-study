f127e425830f33403120f99cea60d2a1155b5169
minkyung kim <minry20415@naver.com>
2024-12-30 23:07:02 +0900

    ✨: 커넥션 풀과 DataSource 이해

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,60 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.domain.Member;
+import lombok.Getter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        // 기본 DriverManager - 항상 새로운 커넥션을 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException, InterruptedException {
+        //save
+        Member member = new Member("memberV101", 10000);
+        Member savedMember = repository.save(member);
+        assertThat(savedMember.getMemberId()).isEqualTo("memberV101");
+
+        //findById
+        Member findMember1 = repository.findById(member.getMemberId());
+        assertThat(findMember1).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 20000);
+        Member findMember2 = repository.findById(member.getMemberId());
+        assertThat(findMember2.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+        Thread.sleep(1000);
+
+    }
+
+}
