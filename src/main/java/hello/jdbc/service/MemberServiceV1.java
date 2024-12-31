4ac5bdb65b274ed68ab9d7fc1a05f2c1ac59be87
minkyung kim <minry20415@naver.com>
2024-12-31 15:41:37 +0900

    ✨: 트랜잭션의 이해

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,37 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    //계좌이체
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        //from money 감소
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        //검증 - 예외 발생
+        validation(toMember);
+
+        //to money 증가
+        memberRepository.update(toId, toMember.getMoney() + money);
+
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+
+}
