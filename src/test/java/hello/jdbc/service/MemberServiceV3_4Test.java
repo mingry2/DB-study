53012c199fe33dd992239c687eb2f85b5cbfe34c
minkyung kim <minry20415@naver.com>
2025-01-03 16:06:40 +0900

    ✨: 스프링과 문제 해결 - 예외 처리, 반복
    - 체크예외 -> 런타임예외 변환
    - 데이터 접근 예외 직접 생성
    - 스프링 예외 추상화의 이해 및 적용
    ㄴ 예외 변환기를 통해 추상화된 데이터접근 예외들을 상황에 맞게 사용할 수 있음
    - JdbcTemplate을 사용하여 JDBC 반복 문제 해결

--- src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
@@ -1,10 +1,7 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_1;
-import hello.jdbc.repository.MemberRepositoryV4_2;
-import hello.jdbc.repository.MemberRepositoryV5;
+import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,27 +12,26 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * 예외 누수 문제 해결
- * SQLException 제거
- * MemberRepository 인터페이스 의존
+ * 트랜잭션 - DataSource, transactionManager 자동 등록
  */
 @Slf4j
 @SpringBootTest
-class MemberServiceV4Test {
+class MemberServiceV3_4Test {
 
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
     @Autowired
-    private MemberRepository memberRepository;
+    private MemberRepositoryV3 memberRepository;
     @Autowired
-    private MemberServiceV4 memberService;
+    private MemberServiceV3_3 memberService;
 
     @TestConfiguration
     static class TestConfig {
@@ -47,20 +43,18 @@ class MemberServiceV4Test {
         }
 
         @Bean
-        MemberRepository memberRepository() {
-//            return new MemberRepositoryV4_1(dataSource);
-//            return new MemberRepositoryV4_2(dataSource);
-            return new MemberRepositoryV5(dataSource);
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource);
         }
 
         @Bean
-        MemberServiceV4 memberServiceV4() {
-            return new MemberServiceV4(memberRepository());
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
         }
     }
 
     @AfterEach
-    void afterEach() {
+    void afterEach() throws SQLException {
         memberRepository.delete(MEMBER_A);
         memberRepository.delete(MEMBER_B);
         memberRepository.delete(MEMBER_EX);
@@ -68,7 +62,7 @@ class MemberServiceV4Test {
 
     @Test
     @DisplayName("정상 이체")
-    void accountTransfer() {
+    void accountTransfer() throws SQLException {
         //given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberB = new Member(MEMBER_B, 10000);
@@ -89,7 +83,7 @@ class MemberServiceV4Test {
 
     @Test
     @DisplayName("이체중 예외 발생")
-    void accountTransferEx() {
+    void accountTransferEx() throws SQLException {
         //given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberEx = new Member(MEMBER_EX, 10000);
